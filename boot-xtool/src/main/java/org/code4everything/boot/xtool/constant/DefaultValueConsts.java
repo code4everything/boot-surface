caef0a5ba38b0be658e76b500c42491685c4b4ef
pantao <tao@zhazhapan.com>
2018-10-30 21:28:09 +0800

    添加字段检查注解

---
+++ boot-xtool/src/main/java/org/code4everything/boot/xtool/constant/DefaultValueConsts.java
@@ -0,0 +1,10 @@
+package org.code4everything.boot.xtool.constant;
+
+/**
+ * @author pantao
+ * @since 2018/10/30
+ */
+public class DefaultValueConsts {
+
+    public static final String ERROR_EN = "error";
+}
