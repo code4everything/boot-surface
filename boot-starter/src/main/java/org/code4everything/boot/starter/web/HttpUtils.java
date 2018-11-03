04d9f91fb260909730ec68c7155209a4fadf75ae
pantao <tao@zhazhapan.com>
2018-11-03 17:34:38 +0800

    可自定义是否对文件进行MD5

--- boot-starter/src/main/java/org/code4everything/boot/starter/web/HttpUtils.java
+++ boot-starter/src/main/java/org/code4everything/boot/starter/web/HttpUtils.java
@@ -2,8 +2,7 @@ package org.code4everything.boot.starter.web;
 
 import cn.hutool.core.lang.Validator;
 import cn.hutool.core.util.StrUtil;
-import cn.hutool.crypto.digest.DigestAlgorithm;
-import cn.hutool.crypto.digest.Digester;
+import cn.hutool.crypto.digest.DigestUtil;
 import org.apache.log4j.Logger;
 import org.code4everything.boot.starter.bean.MultipartFileBean;
 import org.code4everything.boot.starter.service.FileService;
@@ -33,14 +32,16 @@ public class HttpUtils {
      *
      * @param file 文件 {@link MultipartFile}
      * @param storagePath 文件存储路径，如：/root/boot/
+     * @param digestBytes 是否计算文件的MD5码（大文件不建议计算，防止堆内存泄漏）
      * @param <T> 数据表类型
      *
      * @return 响应结果 {@link ResponseResult}。如果上传成功，{@link ResponseResult#getMsg()}返回文件的MD5文件名
      *
      * @since 1.0.0
      */
-    public static <T extends Serializable> ResponseResult<T> upload(MultipartFile file, String storagePath) {
-        return upload(new FileService<T>() {}, file, storagePath, null);
+    public static <T extends Serializable> ResponseResult<T> upload(MultipartFile file, String storagePath,
+                                                                    boolean digestBytes) {
+        return upload(new FileService<T>() {}, file, storagePath, digestBytes, null);
     }
 
     /**
@@ -49,6 +50,7 @@ public class HttpUtils {
      * @param fileService 文件服务 {@link FileService}
      * @param file 文件 {@link MultipartFile}
      * @param storagePath 文件存储路径，如：/root/boot/
+     * @param digestBytes 是否计算文件的MD5码（大文件不建议计算，防止堆内存泄漏）
      * @param <T> 数据表类型
      *
      * @return 响应结果 {@link ResponseResult}。如果文件上传成功且最后得到的 {@link ResponseResult#getData()}为NULL，则{@link
@@ -57,8 +59,8 @@ public class HttpUtils {
      * @since 1.0.0
      */
     public static <T extends Serializable> ResponseResult<T> upload(FileService<T> fileService, MultipartFile file,
-                                                                    String storagePath) {
-        return upload(fileService, file, storagePath, null);
+                                                                    String storagePath, boolean digestBytes) {
+        return upload(fileService, file, storagePath, digestBytes, null);
     }
 
     /**
@@ -67,6 +69,7 @@ public class HttpUtils {
      * @param fileService 文件服务 {@link FileService}
      * @param file 文件 {@link MultipartFile}
      * @param storagePath 文件存储路径，如：/root/boot/
+     * @param digestBytes 是否计算文件的MD5码（大文件不建议计算，防止堆内存泄漏）
      * @param otherParam 附加参数，在自己实现的 {@link FileService}接口中使用
      * @param <T> 数据表类型
      *
@@ -76,21 +79,25 @@ public class HttpUtils {
      * @since 1.0.0
      */
     public static <T extends Serializable> ResponseResult<T> upload(FileService<T> fileService, MultipartFile file,
-                                                                    String storagePath,
+                                                                    String storagePath, boolean digestBytes,
                                                                     Map<String, Serializable> otherParam) {
         ResponseResult<T> result = new ResponseResult<>();
         MultipartFileBean fileBean = new MultipartFileBean();
         // 设置文件信息
         String ofn = file.getOriginalFilename();
-        try {
-            // 设置MD5
-            fileBean.setMd5(new Digester(DigestAlgorithm.MD5).digestHex(file.getBytes()));
-        } catch (Exception e) {
-            LOGGER.error(StrUtil.format("get md5 of file[{}] failed, message -> {}", ofn, e.getMessage()));
-            return result.setCode(IntegerConsts.FOUR_HUNDRED).setMsg(StrUtil.format("{} upload failed", ofn));
+        if (digestBytes) {
+            try {
+                // 设置MD5
+                fileBean.setMd5(DigestUtil.md5Hex(file.getBytes()));
+            } catch (Exception e) {
+                LOGGER.error(StrUtil.format("get md5 of file[{}] failed, message -> {}", ofn, e.getMessage()));
+                return result.setCode(IntegerConsts.FOUR_HUNDRED).setMsg(ofn + " upload failed");
+            }
+            fileBean.setFilename(fileBean.getMd5() + FileUtils.getSuffix(file.getOriginalFilename()));
+        } else {
+            fileBean.setFilename(file.getOriginalFilename());
         }
         fileBean.setOriginalFilename(ofn).setSize(file.getSize()).setOtherParams(otherParam);
-        fileBean.setFilename(fileBean.getMd5() + FileUtils.getSuffix(file.getOriginalFilename()));
         // 检测文件是否存在
         Boolean exists = fileService.exists(fileBean);
         boolean shouldWrite = false;
@@ -111,7 +118,7 @@ public class HttpUtils {
                 file.transferTo(new File(storagePath + fileBean.getFilename()));
             } catch (Exception e) {
                 LOGGER.error("upload file failed, message -> " + e.getMessage());
-                return result.setCode(IntegerConsts.FOUR_HUNDRED).setMsg(StrUtil.format("{} upload failed", ofn));
+                return result.setCode(IntegerConsts.FOUR_HUNDRED).setMsg(ofn + " upload failed");
             }
             // 将数据写入数据库
             t = fileService.save(fileBean);
