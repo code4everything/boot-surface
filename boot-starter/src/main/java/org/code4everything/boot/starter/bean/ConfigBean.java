dd5b0543a12b543c0d8ab51bc0a973601ce2a96d
pantao <tao@zhazhapan.com>
2018-11-04 22:07:33 +0800

    新增默认拦截器

---
+++ boot-starter/src/main/java/org/code4everything/boot/starter/bean/ConfigBean.java
@@ -0,0 +1,106 @@
+package org.code4everything.boot.starter.bean;
+
+import java.io.Serializable;
+
+/**
+ * 配置信息
+ *
+ * @author pantao
+ * @since 2018/11/4
+ */
+public class ConfigBean implements Serializable {
+
+    /**
+     * 黑名单前缀
+     *
+     * @since 1.0.0
+     */
+    private String[] blackPrefixes;
+
+    /**
+     * 白名单前缀
+     *
+     * @since 1.0.0
+     */
+    private String[] whitePrefixes;
+
+    /**
+     * 拦截名单前缀
+     *
+     * @since 1.0.0
+     */
+    private String[] interceptPrefixes;
+
+    /**
+     * 获取黑名单前缀
+     *
+     * @return 黑名单前缀
+     */
+    public String[] getBlackPrefixes() {
+        return blackPrefixes;
+    }
+
+    /**
+     * 设置黑名单前缀
+     *
+     * @param blackPrefixes 黑名单前缀
+     *
+     * @return {@link ConfigBean}
+     *
+     * @since 1.0.0
+     */
+    public ConfigBean setBlackPrefixes(String[] blackPrefixes) {
+        this.blackPrefixes = blackPrefixes;
+        return this;
+    }
+
+    /**
+     * 获取白名单前缀
+     *
+     * @return 白名单前缀
+     *
+     * @since 1.0.0
+     */
+    public String[] getWhitePrefixes() {
+        return whitePrefixes;
+    }
+
+    /**
+     * 设置白名单前缀
+     *
+     * @param whitePrefixes 白名单前缀
+     *
+     * @return {@link ConfigBean}
+     *
+     * @since 1.0.0
+     */
+    public ConfigBean setWhitePrefixes(String[] whitePrefixes) {
+        this.whitePrefixes = whitePrefixes;
+        return this;
+    }
+
+    /**
+     * 获取拦截名单前缀
+     *
+     * @return 拦截名单前缀
+     *
+     * @since 1.0.0
+     */
+    public String[] getInterceptPrefixes() {
+        return interceptPrefixes;
+    }
+
+    /**
+     * 设置拦截名单前缀
+     *
+     * @param interceptPrefixes 拦截名单前缀
+     *
+     * @return {@link ConfigBean}
+     *
+     * @since 1.0.0
+     */
+    public ConfigBean setInterceptPrefixes(String[] interceptPrefixes) {
+        this.interceptPrefixes = interceptPrefixes;
+        return this;
+    }
+}
