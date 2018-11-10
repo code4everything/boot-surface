065809bd603420b4f1bb8fa5c0fef2be088cf439
pantao <tao@zhazhapan.com>
2018-11-10 11:50:08 +0800

    新增文件监听

--- src/main/java/org/code4everything/boot/base/FileUtils.java
+++ src/main/java/org/code4everything/boot/base/FileUtils.java
@@ -1,8 +1,15 @@
 package org.code4everything.boot.base;
 
+import cn.hutool.core.io.watch.SimpleWatcher;
+import cn.hutool.core.io.watch.WatchMonitor;
+import cn.hutool.core.io.watch.Watcher;
+import cn.hutool.core.io.watch.watchers.DelayWatcher;
 import cn.hutool.core.util.StrUtil;
+import org.code4everything.boot.interfaces.FileWatcher;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.WatchEvent;
 
 /**
  * 文件工具类
@@ -13,6 +20,56 @@ import java.nio.file.Paths;
 public class FileUtils {
 
     private FileUtils() {}
+
+    /**
+     * 监听文件
+     *
+     * @param file 待监听的文件
+     * @param fileWatcher {@link FileWatcher}
+     *
+     * @since 1.0.0
+     */
+    public static void watchFile(String file, FileWatcher fileWatcher) {
+        watchFile(file, fileWatcher, false);
+    }
+
+    /**
+     * 监听文件
+     *
+     * @param file 待监听的文件
+     * @param fileWatcher {@link FileWatcher}
+     * @param shouldFirstExecute 加载时是否执行 {@link FileWatcher#doSomething()}
+     *
+     * @since 1.0.0
+     */
+    public static void watchFile(String file, FileWatcher fileWatcher, boolean shouldFirstExecute) {
+        if (shouldFirstExecute) {
+            fileWatcher.doSomething();
+        }
+        SimpleWatcher simpleWatcher = new SimpleWatcher() {
+            @Override
+            public void onModify(WatchEvent<?> event, Path currentPath) {
+                fileWatcher.onModify(event, currentPath);
+                fileWatcher.doSomething();
+            }
+        };
+        // 文件发生变化时延迟一秒钟执行
+        watchFile(file, new DelayWatcher(simpleWatcher, 1000));
+    }
+
+    /**
+     * 监听文件
+     *
+     * @param file 待监听的文件
+     * @param watcher 监听器 {@link Watcher}
+     *
+     * @since 1.0.0
+     */
+    public static void watchFile(String file, Watcher watcher) {
+        try (WatchMonitor monitor = WatchMonitor.createAll(file, watcher)) {
+            monitor.start();
+        }
+    }
 
     /**
      * 获取当前工作路径
---
+++ src/main/java/org/code4everything/boot/interfaces/FileWatcher.java
@@ -0,0 +1,30 @@
+package org.code4everything.boot.interfaces;
+
+import java.nio.file.Path;
+import java.nio.file.WatchEvent;
+
+/**
+ * 文件监听器
+ *
+ * @author pantao
+ * @since 2018/11/10
+ */
+public interface FileWatcher {
+
+    /**
+     * 执行一些业务代码
+     *
+     * @since 1.0.0
+     */
+    default void doSomething() {}
+
+    /**
+     * 文件发生变化
+     *
+     * @param event 时间
+     * @param currentPath 当前路径
+     *
+     * @since 1.0.0
+     */
+    default void onModify(WatchEvent<?> event, Path currentPath) {}
+}
