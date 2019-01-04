8d7321b05e541f3531f487eb2fde76d256a3f253
pantao <tao@zhazhapan.com>
2019-01-04 11:59:19 +0800

    添加常量

--- src/main/java/org/code4everything/boot/constant/IntegerConsts.java
+++ src/main/java/org/code4everything/boot/constant/IntegerConsts.java
@@ -16,6 +16,22 @@ public class IntegerConsts {
 
     public static final int ZERO = 0;
 
+    public static final int ONE = 1;
+
+    public static final int TWO = 2;
+
+    public static final int THREE = 3;
+
+    public static final int FOUR = 4;
+
+    public static final int SIX = 6;
+
+    public static final int SEVEN = 7;
+
+    public static final int NINE = 9;
+
+    public static final int TEN = 10;
+
     private IntegerConsts() {}
 
     public class FileSize {
