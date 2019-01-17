45a6c976780f409040ac88d13859e0e81cc53b3a
pantao <tao@zhazhapan.com>
2019-01-17 20:44:12 +0800

    排序列表

--- src/main/java/org/code4everything/boot/base/collection/SortedList.java
+++ src/main/java/org/code4everything/boot/base/collection/SortedList.java
@@ -2,6 +2,7 @@ package org.code4everything.boot.base.collection;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * 排序集合
@@ -9,48 +10,112 @@ import java.util.List;
  * @author pantao
  * @since 2019/1/17
  **/
-public class SortedList<T extends List<E>, E extends Comparable> {
+public class SortedList<E extends Comparable, T extends List<E>> {
 
+    /**
+     * 数据源
+     *
+     * @since 1.0.6
+     */
     private T list;
 
+    /**
+     * 比较器
+     *
+     * @since 1.0.6
+     */
     private Comparator<E> comparator;
 
+    /**
+     * 构造函数
+     *
+     * @param list 数据源
+     *
+     * @since 1.0.6
+     */
     public SortedList(T list) {
+        setList(list);
+    }
+
+    /**
+     * 构造函数
+     *
+     * @param list 数据源
+     * @param comparator 比较器
+     *
+     * @since 1.0.6
+     */
+    public SortedList(T list, Comparator<E> comparator) {
+        setList(list, comparator);
+    }
+
+    /**
+     * 添加元素，忽略空值
+     *
+     * @param e 数据
+     *
+     * @since 1.0.6
+     */
+    public void add(E e) {
+        if (Objects.isNull(e)) {
+            return;
+        }
+        int idx = 0;
+        for (int i = list.size() - 1; i >= 0; i--) {
+            int compare;
+            if (comparator == null) {
+                compare = e.compareTo(list.get(i));
+            } else {
+                compare = comparator.compare(list.get(i), e);
+            }
+            if (compare >= 0) {
+                idx = i + 1;
+                break;
+            }
+        }
+        list.add(idx, e);
+    }
+
+    /**
+     * 获取数据源
+     *
+     * @return {@link T}
+     *
+     * @since 1.0.6
+     */
+    public T getList() {
+        return list;
+    }
+
+    /**
+     * 设置数据源
+     *
+     * @param list 数据源
+     *
+     * @since 1.0.6
+     */
+    @SuppressWarnings("unchecked")
+    public void setList(T list) {
         if (list == null) {
             throw new NullPointerException();
         }
         if (list.size() > 1) {
-            list.forEach(this::sort);
+            // 与比较器相反
+            list.sort(comparator == null ? Comparator.naturalOrder() : comparator.reversed());
         }
         this.list = list;
     }
 
-    public void add(E e) {
-        sort(e);
-    }
-
-    private void sort(E e) {
-        if (this.list.isEmpty()) {
-            list.add(e);
-        } else {
-            int idx = 0;
-            for (int i = list.size() - 1; i >= 0; i--) {
-                int compare = 0;
-                if (comparator == null) {
-                    compare = e.compareTo(list.get(i));
-                } else {
-                    compare = comparator.compare(list.get(i), e);
-                }
-                if (compare >= 0) {
-                    idx = i + 1;
-                    break;
-                }
-            }
-            list.add(idx, e);
-        }
-    }
-
-    public T getList() {
-        return list;
+    /**
+     * 设置数据源和比较器
+     *
+     * @param list 数据源
+     * @param comparator 比较器
+     *
+     * @since 1.0.6
+     */
+    public void setList(T list, Comparator<E> comparator) {
+        this.comparator = comparator;
+        setList(list);
     }
 }
--- src/test/java/org/code4everything/boot/base/collection/SortedCollectionTest.java
+++ src/test/java/org/code4everything/boot/base/collection/SortedCollectionTest.java
@@ -4,16 +4,38 @@ import cn.hutool.core.util.RandomUtil;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class SortedCollectionTest {
 
     @Test
     public void getCollection() {
-        SortedList<List<Integer>, Integer> sortedList = new SortedList<>(new ArrayList<>());
-        for (int i = 0; i < 100; i++) {
+        // 升序排列
+        SortedList<Integer, List<Integer>> sortedList = new SortedList<>(new ArrayList<>(), (i1, i2) -> i2 - i1);
+        for (int i = 0; i < 1000; i++) {
             sortedList.add(RandomUtil.randomInt(999));
         }
         System.out.println(sortedList.getList());
+        // 测试已有数据
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            list.add(RandomUtil.randomInt(999));
+        }
+        sortedList.setList(list);
+        System.out.println(list);
+        // 降序排列
+        sortedList.setList(new ArrayList<>(), Comparator.comparingInt(i -> i));
+        for (int i = 0; i < 1000; i++) {
+            sortedList.add(RandomUtil.randomInt(999));
+        }
+        System.out.println(sortedList.getList());
+        // 测试已有数据
+        list = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            list.add(RandomUtil.randomInt(999));
+        }
+        sortedList.setList(list, Comparator.comparingInt(o -> o));
+        System.out.println(list);
     }
 }
