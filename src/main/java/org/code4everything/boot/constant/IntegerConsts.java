8057019450e4bc28ca670fac02fd1754e06fe474
pantao <tao@zhazhapan.com>
2019-02-02 15:11:48 +0800

    新增常量

--- src/main/java/org/code4everything/boot/constant/IntegerConsts.java
+++ src/main/java/org/code4everything/boot/constant/IntegerConsts.java
@@ -32,6 +32,8 @@ public class IntegerConsts {
 
     public static final int TEN = 10;
 
+    public static final int ONE_DAY_MILLIS = 24 * 60 * 60 * 1000;
+
     private IntegerConsts() {}
 
     public class FileSize {
