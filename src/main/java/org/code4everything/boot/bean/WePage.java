973ea1d78433015479b172735e91d0d4295404b9
pantao <tao@zhazhapan.com>
2019-03-21 12:03:41 +0800

    优化分页

--- src/main/java/org/code4everything/boot/bean/WePage.java
+++ src/main/java/org/code4everything/boot/bean/WePage.java
@@ -1,5 +1,6 @@
 package org.code4everything.boot.bean;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.ObjectUtil;
 
 import java.io.IOException;
@@ -21,7 +22,9 @@ public class WePage<T> implements BaseBean, Serializable {
 
     private Integer pageSize;
 
-    private Integer totalPage;
+    private Integer totalPages;
+
+    private Long totalElements;
 
     private transient Collection<T> content;
 
@@ -31,17 +34,166 @@ public class WePage<T> implements BaseBean, Serializable {
     public WePage() {}
 
     /**
+     * 构造方法
+     *
      * @param currPage 当前页
      * @param pageSize 页大小
-     * @param totalPage 总页数
      *
      * @since 1.0.9
      */
-    public WePage(Integer currPage, Integer pageSize, Integer totalPage) {
+    public WePage(Integer currPage, Integer pageSize) {
         this.currPage = currPage;
         this.pageSize = pageSize;
-        this.totalPage = totalPage;
         content = new ArrayList<>();
+    }
+
+    /**
+     * 构造方法
+     *
+     * @param currPage 当前页
+     * @param pageSize 页大小
+     * @param totalPages 总页数
+     *
+     * @since 1.0.9
+     */
+    public WePage(Integer currPage, Integer pageSize, Integer totalPages) {
+        this.currPage = currPage;
+        this.pageSize = pageSize;
+        this.totalPages = totalPages;
+        content = new ArrayList<>();
+    }
+
+    /**
+     * 构造方法
+     *
+     * @param currPage 当前页
+     * @param pageSize 页大小
+     * @param totalElements 总元素数量
+     *
+     * @since 1.0.9
+     */
+    public WePage(Integer currPage, Integer pageSize, Long totalElements) {
+        this.currPage = currPage;
+        this.pageSize = pageSize;
+        this.totalElements = totalElements;
+        computeTotalPage();
+        content = new ArrayList<>();
+    }
+
+    /**
+     * 构造方法
+     *
+     * @param currPage 当前页
+     * @param pageSize 页大小
+     * @param content 元素列表
+     *
+     * @since 1.0.9
+     */
+    public WePage(Integer currPage, Integer pageSize, Collection<T> content) {
+        this.currPage = currPage;
+        this.pageSize = pageSize;
+        this.content = content;
+    }
+
+    /**
+     * 构造方法
+     *
+     * @param currPage 当前页
+     * @param pageSize 页大小
+     * @param totalElements 总元素数量
+     * @param content 元素列表
+     *
+     * @since 1.0.9
+     */
+    public WePage(Integer currPage, Integer pageSize, Long totalElements, Collection<T> content) {
+        this.currPage = currPage;
+        this.pageSize = pageSize;
+        this.totalElements = totalElements;
+        computeTotalPage();
+        this.content = content;
+    }
+
+    /**
+     * 构造方法
+     *
+     * @param currPage 当前页
+     * @param pageSize 页大小
+     * @param totalElements 总元素数量
+     * @param content 元素列表
+     *
+     * @since 1.0.9
+     */
+    public WePage(Integer currPage, Integer pageSize, Integer totalElements, Collection<T> content) {
+        this.currPage = currPage;
+        this.pageSize = pageSize;
+        this.totalElements = Long.valueOf(totalElements);
+        computeTotalPage();
+        this.content = content;
+    }
+
+
+    /**
+     * 获取当前页元素数量
+     *
+     * @return 当前页元素数量
+     *
+     * @since 1.0.9
+     */
+    public Integer getElementSize() {
+        return CollUtil.isEmpty(content) ? 0 : content.size();
+    }
+
+    /**
+     * 获取总元素数量
+     *
+     * @return 总元素数量
+     *
+     * @since 1.0.9
+     */
+    public Long getTotalElements() {
+        return totalElements;
+    }
+
+    /**
+     * 设置总元素数量，总页数会发生响应的变化
+     *
+     * @param totalElements 总元素数量
+     *
+     * @return {@link WePage}
+     *
+     * @since 1.0.9
+     */
+    public WePage<T> setTotalElements(Long totalElements) {
+        this.totalElements = totalElements;
+        computeTotalPage();
+        return this;
+    }
+
+    /**
+     * 设置总元素数量，总页数会发生响应的变化
+     *
+     * @param totalElements 总元素数量
+     *
+     * @return {@link WePage}
+     *
+     * @since 1.0.9
+     */
+    public WePage<T> setTotalElements2(Integer totalElements) {
+        this.totalElements = Long.valueOf(totalElements);
+        computeTotalPage();
+        return this;
+    }
+
+
+    /**
+     * 计算总页数
+     *
+     * @since 1.0.9
+     */
+    private void computeTotalPage() {
+        if (ObjectUtil.isNotNull(pageSize) && pageSize > 0 && ObjectUtil.isNotNull(totalElements) && totalElements >= 0) {
+            totalPages = (int) (totalElements / pageSize) + (totalElements % pageSize > 0 ? 1 : 0);
+        }
     }
 
     /**
@@ -91,6 +243,7 @@ public class WePage<T> implements BaseBean, Serializable {
      */
     public WePage<T> setPageSize(Integer pageSize) {
         this.pageSize = pageSize;
+        computeTotalPage();
         return this;
     }
 
@@ -101,21 +254,21 @@ public class WePage<T> implements BaseBean, Serializable {
      *
      * @since 1.0.9
      */
-    public Integer getTotalPage() {
-        return totalPage;
+    public Integer getTotalPages() {
+        return totalPages;
     }
 
     /**
      * 设置总页数
      *
-     * @param totalPage 总页数
+     * @param totalPages 总页数
      *
      * @return 总页数
      *
      * @since 1.0.9
      */
-    public WePage<T> setTotalPage(Integer totalPage) {
-        this.totalPage = totalPage;
+    public WePage<T> setTotalPages(Integer totalPages) {
+        this.totalPages = totalPages;
         return this;
     }
 
@@ -160,15 +313,16 @@ public class WePage<T> implements BaseBean, Serializable {
 
     @Override
     public String toString() {
-        return "WePage{" + "currPage=" + currPage + ", pageSize=" + pageSize + ", totalPage=" + totalPage + ", " +
-                "content=" + content + '}';
+        return "WePage{" + "currPage=" + currPage + ", pageSize=" + pageSize + ", totalPages=" + totalPages + ", " +
+                "totalElements=" + totalElements + ", content=" + content + '}';
     }
 
     private void writeObject(ObjectOutputStream outputStream) throws IOException {
         outputStream.defaultWriteObject();
         outputStream.writeInt(currPage);
         outputStream.writeInt(pageSize);
-        outputStream.writeInt(totalPage);
+        outputStream.writeInt(totalPages);
+        outputStream.writeLong(totalElements);
         outputStream.writeObject(ObjectUtil.serialize(content));
     }
 
@@ -176,7 +330,8 @@ public class WePage<T> implements BaseBean, Serializable {
         inputStream.defaultReadObject();
         currPage = inputStream.readInt();
         pageSize = inputStream.readInt();
-        totalPage = inputStream.readInt();
+        totalPages = inputStream.readInt();
+        totalElements = inputStream.readLong();
         content = ObjectUtil.unserialize((byte[]) inputStream.readObject());
     }
 }
