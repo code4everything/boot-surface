54e7d64d14fda340d7967067fc0eb1734a6500c0
pantao <easepan@qq.com>
2019-04-12 18:21:14 +0800

    新增接口

--- src/main/java/org/code4everything/boot/service/BootUserService.java
+++ src/main/java/org/code4everything/boot/service/BootUserService.java
@@ -1,5 +1,7 @@
 package org.code4everything.boot.service;
 
+import java.util.Map;
+
 /**
  * 用户服务
  *
@@ -18,4 +20,55 @@ public interface BootUserService<T> {
      * @since 1.0.4
      */
     T getUserByToken(String token);
+
+    /**
+     * 从缓存中移除
+     *
+     * @param token Token
+     *
+     * @since 1.1.0
+     */
+    default void removeFromCache(String token) {removeFromCache(token, null);}
+
+    /**
+     * 从缓存中移除
+     *
+     * @param token Token
+     * @param user 用户
+     *
+     * @since 1.1.0
+     */
+    default void removeFromCache(String token, T user) {}
+
+    /**
+     * 存入缓存
+     *
+     * @param token Token
+     * @param user 用户
+     *
+     * @since 1.1.0
+     */
+    default void put2cache(String token, T user) {}
+
+    /**
+     * 自定义查询
+     *
+     * @param params 参数集合
+     *
+     * @return 用户
+     *
+     * @since 1.1.0
+     */
+    default T getUserBy(Map<String, String> params) {return null;}
+
+    /**
+     * 自定义查询
+     *
+     * @param params 参数集合
+     *
+     * @return 用户
+     *
+     * @since 1.1.0
+     */
+    default T getUserBy(String... params) {return null;}
 }
