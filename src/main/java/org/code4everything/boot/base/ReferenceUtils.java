a5a952c30ce898d086af00bfa46bbdc627fe4e4d
pantao <easepan@qq.com>
2019-06-27 15:51:48 +0800

    feat: remove sso

--- src/main/java/org/code4everything/boot/base/ReferenceUtils.java
+++ src/main/java/org/code4everything/boot/base/ReferenceUtils.java
@@ -14,7 +14,7 @@ public class ReferenceUtils {
     private ReferenceUtils() {}
 
     /**
-     * 获取引用值
+     * 安全的获取引用值
      *
      * @param reference 值的引用
      * @param <T> 值类型
@@ -23,7 +23,7 @@ public class ReferenceUtils {
      *
      * @since 1.1.5
      */
-    public static <T> T safeUnwrap(Reference<T> reference) {
+    public static <T> T unwrap(Reference<T> reference) {
         return Objects.isNull(reference) ? null : reference.get();
     }
 }
--- src/main/java/org/code4everything/boot/web/sso/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * 单点登录的简单封装
- *
- * @since 1.1.5
- */
-package org.code4everything.boot.web.sso;
